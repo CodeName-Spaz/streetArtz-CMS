94bd1b7c484814f6b89c6a6e36fcd1e1cdceebe6
Leonard <spazgrafficz@outlook.com>
2019-02-08 08:43:45 +0200

    changes

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 declare var firebase;
 
 
@@ -24,60 +25,21 @@ export class SignInComponent implements OnInit {
   login(email, password) {
      if (email == "" || email == undefined && password == "" || password == undefined) {
        alert("Please check your fields")
-      // this.alertMessage = "Please insert your email address and password to sign in.";
-      // myAlert[0].style.display = "block";
-      // theLoader[0].style.display = "none"
 
     } else
       if (email == "" || email == undefined) {
         alert("Please insert your email correectly")
-        // this.alertMessage = "Please insert your email address";
-        // myAlert[0].style.display = "block";
-        // theLoader[0].style.display = "none"
-        // alert('no email')
       }
       else if (password == "" || password == undefined) {
         alert("Please insert your password correctly")
-        // this.alertMessage = "Please insert your password";
-        // myAlert[0].style.display = "block";
-        // theLoader[0].style.display = "none"
-        // alert('no pass')
       }
       else {
-
-        // this.alertMessage = "Signing in...";
-        // theOK.style.display = "none";
-        // leader[0].style.display = "block"
         firebase.auth().signInWithEmailAndPassword(email, password).then(() =>{
             alert("Welcome")
-            // myAlert[0].style.top = (b/3.5) + "px";
-            // myAlert[0].style.left = "2.3%"; 
-            // alert("logged in")
             this.router.navigate(['/landing'])
         }, Error => {
           alert("something's wrong")
-          // alert(Error.message);
-          // console.log(Error.message);
-        //   myAlert[0].style.display = "block";
-        //   theLoader[0].style.display = "none"
-        //   if (Error.message == "There is no user record corresponding to this identifier. The user may have been deleted.") {
-        //     this.alertMessage = "We do not have a record of this email address, please check your email address or sign up and get started..."
-        //   }
-        //   else if (Error.message == "The password is invalid or the user does not have a password.") {
-        //     this.alertMessage = "Please ensure that your password is correct."
-        //   }
-        //   else if (Error.message == "The email address is badly formatted.") {
-        //     this.alertMessage = "Please check if your email address is correct, something's not right."
-        //   }
-        //   else {
-        //     this.alertMessage = Error.message;
-        //   }
-        //   // theOK.style.display = "block";
-        //   // leader[0].style.display = "none";
-        // })
-      // }
-
-  // }
+   
 
   })
 }
